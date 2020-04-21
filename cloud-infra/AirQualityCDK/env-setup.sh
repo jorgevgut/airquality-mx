c9157b693f4fc2d6ca44580ef39bb358ea92c427
Jorge A Villalobos G (aka Latincoder) <dev.javg@gmail.com>
2020-04-21 11:53:54 -0700

    Adding helper script to setup required env vards
    Use this appropiatly to generate cloud infrastructure with correct
    values.

---
+++ cloud-infra/AirQualityCDK/env-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash.exe
+export TOKEN="replace_this"
+export TWITTER_APIK="replace this";
+export TWITTER_APIS="replace this";
+export TWITTER_ACCESS_TOKEN="replace this";
+export TWITTER_SECRET_TOKEN="replace this";
